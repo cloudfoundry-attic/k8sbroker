24f47054dbef453051a8bd7c4e6527a158c52e82
Dave Walter <dwalter@pivotal.io>
2018-11-30 14:23:09 -0800

    Update deploy script
    - adds support for flags to avoid positional parameters
    - adds app-domain for broker registration
    - expands usage output
    
    [#161269364](https://www.pivotaltracker.com/story/show/#161269364)
    
    Co-authored-by: Paul Warren <paul.warren@emc.com>

--- README.md
+++ README.md
@@ -9,15 +9,21 @@ The k8sbroker is a golang application that can be `cf push`ed to an Eirini deplo
 To deploy the broker and enable its services in the cf marketplace:
 
 ```
-$ CF_USERNAME=<username> CF_PASSWORD=<password> ./scripts/deploy.sh <path-to-kube-config.json> <cf-api> <org> <space>
+$ CF_USERNAME=<username> CF_PASSWORD=<password> ./scripts/deploy.sh \
+  -k <path-to-kube-config.json> \
+  -i <cf-api> \
+  -o <org> \
+  -s <space> \
+  -a <app-domain>
 ```
 
 where:
-- path-to-kube-config.json: is the path to a valid kube config json file that allows connection to the kubernetes cluster 
+- path-to-kube-config.json: is the path to a valid kube config json file that allows connection to the kubernetes cluster
   from within the cluster itself.  This file will be used by the k8sbroker to manage persistent volumes and persistent volume claims.
 - cf-api: the cf api endpoint through which the k8sbroker will be pushed
 - org: the org that the k8sbroker will be psuhed into
 - space: the space that the k8sbroker will be pushed into
+- app-domain: the application domain for the CF deployment
 
 ## Using the k8sbroker
 
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,59 +2,95 @@
 
 set -e
 
+function fail() {
+    echo "ERROR: $*"
+    echo
+    usage
+    exit 1
+}
+
 function usage() {
-    cat <<EOT
-Usage: $(basename $0) <path-to-kube-config> <cf-api-url> <cf-org-name> <cf-space-name>
+  cat <<EOT
+Usage:
+  $(basename $0) <options>
+
+Options:
+  -a <app-domain>          - CF app domain
+  -i <api-url>             - CF API URL
+  -k <path-to-kube-config> - Path to kube-config
+  -o <cf-org-name>         - CF org to target
+  -s <cf-space-url>        - CF space to target
+
+Environment:
+  CF_USERNAME              - CF admin username
+  CF_PASSWORD              - CF admin password
 EOT
 }
 
 function main() {
+    local o apiEndpoint appDomain cfOrg cfSpace kubeConfig
+
+    while getopts a:hi:k:o:s: o; do
+        case $o in
+            a)
+                appDomain="${OPTARG}"
+                ;;
+            h)
+                usage
+                exit 0
+                ;;
+            i)
+                apiEndpoint="${OPTARG}"
+                ;;
+            k)
+                kubeConfig="${OPTARG}"
+                ;;
+            o)
+                cfOrg="${OPTARG}"
+                ;;
+            s)
+                cfSpace="${OPTARG}"
+                ;;
+            *)
+                echo "Unsupported flag: $o"
+                ;;
+        esac
+    done
+
+    shift $((OPTIND-1))
+
     cd "$(dirname "$0")/.."
 
-    local kubeConfig
-    kubeConfig="$1"
-
-    if [[ ! -r "${kubeConfig}" ]]; then
-        echo "ERROR: Cannot read Kube config: ${kubeConfig}"
-        usage
-        exit 1
+    if [[ -z "${appDomain}" ]]; then
+        fail "Please provide the CF app domain."
     fi
-
-    local apiEndpoint
-    apiEndpoint="$2"
 
     if [[ -z "${apiEndpoint}" ]]; then
-        echo "ERROR: Please provide the CF API URL to target."
-        usage
-        exit 1
+        fail "Please provide the CF API URL to target."
     fi
 
-    local cfOrg
-    cfOrg="$3"
+    if [[ -z "${kubeConfig}" ]]; then
+        fail "Please provide the path to a valid kube-config file."
+    fi
+
+    if [[ ! -r "${kubeConfig}" ]]; then
+        fail "Cannot read kube config: ${kubeConfig}"
+    fi
 
     if [[ -z "${cfOrg}" ]]; then
-        echo "ERROR: Please provide the CF org to target."
-        usage
-        exit 1
+        fail "Please provide the CF org to target."
     fi
 
-    local cfSpace
-    cfSpace="$4"
-
     if [[ -z "${cfSpace}" ]]; then
-        echo "ERROR: Please provide the CF space to target."
-        usage
-        exit 1
+        fail "Please provide the CF space to target."
     fi
 
     if [[ -z "${CF_USERNAME}" ]]; then
-        echo "ERROR: Please set CF_USERNAME for cf cli authentication."
-        exit 1
+        fail "Please set CF_USERNAME for cf cli authentication."
     fi
 
     if [[ -z "${CF_PASSWORD}" ]]; then
-        echo "ERROR: Please set CF_PASSWORD for cf cli authentication."
-        exit 1
+        fail "Please set CF_PASSWORD for cf cli authentication."
     fi
 
     cp "${kubeConfig}" kube_config.json
@@ -81,6 +117,12 @@ function main() {
 
     echo "Pushing k8sbroker app..."
     cf push k8sbroker
+
+    echo "Registering the broker..."
+    cf create-service-broker k8sbroker admin admin "https://k8sbroker.${appDomain}"
+
+    echo "Enabling service access..."
+    cf enable-service-access nfs
 }
 
 main "$@"
